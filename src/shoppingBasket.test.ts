705edbf6750a440b69b2fabaadd112d63f013a18
Silviu Eigel <sheigel@sheigel.local>
2017-02-18 15:09:41 +0200

    many simplifications

--- src/shoppingBasket.test.ts
+++ src/shoppingBasket.test.ts
@@ -7,34 +7,34 @@ describe('shopping cart', () => {
     test('book list should be an array', () => {
         expect(() => calculateCost('some value' as any)).toThrowError('Book list should be an array')
     })
-    test(`1 book costs 8`, () => {
+    test(`1 book: costs 8`, () => {
         expect(calculateCost(['Harry Potter I'])).toEqual(8)
     })
-    test(`2 different books have discount of 5%`, () => {
+    test(`2 books: 2 different books get 5% discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter II'])).toEqual(15.2)
     })
-    test(`2 identical books do not have a discount`, () => {
+    test(`2 books: 2 identical books no discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter I'])).toEqual(16)
     })
-    test(`3 identical books do not have a discount`, () => {
+    test(`3 books: 3 identical books no discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter I', 'Harry Potter I'])).toEqual(24)
     })
-    test(`3 books: 2 different get a 5% discount, 1 gets no discount`, () => {
+    test(`3 books: 2 different get 5% discount, 1 no discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter II'])).toEqual(23.2)
     })
-    test(`3 different books have a 10% discount`, () => {
+    test(`3 books: 3 different books get 10% discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter III'])).toEqual(21.6)
     })
-    test(`4 different books have a 20% discount`, () => {
+    test(`4 books: 4 different books get 20% discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter III', 'Harry Potter IV'])).toEqual(25.6)
     })
-    test(`4 books: 2 different get 5% discount, 2 duplicate get no discount`, () => {
+    test(`4 books: 2 different get 5% discount, 2 duplicate no discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter I', 'Harry Potter II'])).toEqual(31.2)
     })
-    test(`4 books: 3 different get 10% discount, 1 duplicate gets no discount`, () => {
+    test(`4 books: 3 different get 10% discount, 1 duplicate no discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter III', 'Harry Potter II'])).toEqual(29.6)
     })
-    test(`5 different books get 25% discount`, () => {
+    test(`5 books: 5 different books get 25% discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter III', 'Harry Potter IV', 'Harry Potter V'])).toEqual(30)
     })
     test(`complete basket discount`, () => {
--- src/shoppingBasket.ts
+++ src/shoppingBasket.ts
@@ -1,31 +1,36 @@
 const BOOK_PRICE = 8
-const BOOK_DISCOUNTS={
-    0:0,
-    1:0,
-    2:0.05,
-    3:0.10,
-    4:0.20,
-    5:0.25
+const BOOK_DISCOUNTS = {
+    0: 0,
+    1: 0,
+    2: 0.05,
+    3: 0.10,
+    4: 0.20,
+    5: 0.25
 }
+
 function discount(uniqueBooksCount: number) {
     return BOOK_DISCOUNTS[uniqueBooksCount]
 }
 
+function uniqueBooks(bookList) {
+    return new Set<string>(bookList)
+}
 
 export default function calculateCost(bookList: Array<string>) {
     if (!(bookList instanceof Array)) {
         throw new Error("Book list should be an array")
     }
 
-    const booksCount = bookList.length
-    const uniqueBooks = Object.keys(bookList.reduce((acc, el)=>{
-        acc[el] = null
-        return acc
-    }, {}))
-    const uniqueBooksCount = uniqueBooks.length
-    if (booksCount === uniqueBooksCount) {
-        return BOOK_PRICE * booksCount * (1 - discount(uniqueBooksCount))
+    function uniqueBooksCount() {
+        return uniqueBooks(bookList).size
     }
-    return BOOK_PRICE * uniqueBooksCount * (1 - discount(uniqueBooksCount)) +
-        (booksCount - uniqueBooksCount) * BOOK_PRICE
+
+    function allBooksCount() {
+        return bookList.length
+    }
+
+    return BOOK_PRICE * (
+            uniqueBooksCount() * (1 - discount(uniqueBooksCount())) +
+            (allBooksCount() - uniqueBooksCount())
+        )
 }
