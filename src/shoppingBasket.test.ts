ed7e3a56520ef66823780d987c61f23615b2eacc
Silviu Eigel <sheigel@sheigel.local>
2017-02-18 14:19:47 +0200

    temporary last test attempt

--- src/shoppingBasket.test.ts
+++ src/shoppingBasket.test.ts
@@ -19,7 +19,30 @@ describe('shopping cart', () => {
     test(`3 identical books do not have a discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter I', 'Harry Potter I'])).toEqual(24)
     })
+    test(`3 books: 2 different get a 5% discount, 1 gets no discount`, () => {
+        expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter II'])).toEqual(23.2)
+    })
     test(`3 different books have a 10% discount`, () => {
         expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter III'])).toEqual(21.6)
+    })
+    test(`4 different books have a 20% discount`, () => {
+        expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter III', 'Harry Potter IV'])).toEqual(25.6)
+    })
+    test(`4 books: 2 different get 5% discount, 2 duplicate get no discount`, () => {
+        expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter I', 'Harry Potter II'])).toEqual(30.4)
+    })
+    test(`4 books: 3 different get 10% discount, 1 duplicate gets no discount`, () => {
+        expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter III', 'Harry Potter II'])).toEqual(29.6)
+    })
+    test(`5 different books get 25% discount`, () => {
+        expect(calculateCost(['Harry Potter I', 'Harry Potter II', 'Harry Potter III', 'Harry Potter IV', 'Harry Potter V'])).toEqual(30)
+    })
+    test(`complete basket discount`, () => {
+        expect(calculateCost([
+            'Harry Potter I','Harry Potter I',
+            'Harry Potter II', 'Harry Potter II',
+            'Harry Potter III', 'Harry Potter III',
+            'Harry Potter IV',
+            'Harry Potter V'])).toEqual(51.6)
     })
 })
--- src/shoppingBasket.ts
+++ src/shoppingBasket.ts
@@ -1,13 +1,36 @@
 const BOOK_PRICE = 8
-
+const BOOK_DISCOUNTS = {
+    0: 0,
+    1: 0,
+    2: 0.05,
+    3: 0.10,
+    4: 0.20,
+    5: 0.25
+}
 function discount(uniqueBooksCount: number) {
-    if (uniqueBooksCount === 3) {
-        return 0.10
+    return BOOK_DISCOUNTS[uniqueBooksCount]
+}
+
+function bookSeries(bookList: Array<string>) {
+    const series = []
+
+    function getBookSeries(book) {
+        for (let sIdx = 0; sIdx < series.length; sIdx++) {
+            if (!series[sIdx][book]) {
+                return series[sIdx]
+            }
+        }
+        const currentSeries = {}
+        series.push(currentSeries);
+        return currentSeries;
     }
-    if (uniqueBooksCount === 2) {
-        return  0.05
+
+    for (let bIdx = 0; bIdx < bookList.length; bIdx++) {
+        const book = bookList[bIdx];
+        getBookSeries(book)[book]=book;
     }
-    return 0
+    console.log('the series',series)
+    return series;
 }
 
 export default function calculateCost(bookList: Array<string>) {
@@ -16,9 +39,10 @@ export default function calculateCost(bookList: Array<string>) {
     }
 
     const booksCount = bookList.length
-    const uniqueBooks = bookList.filter(book =>
-        bookList.indexOf(book) === bookList.lastIndexOf(book)
-    )
-    const uniqueBooksCount = uniqueBooks.length
-    return BOOK_PRICE * booksCount * (1 - discount(uniqueBooksCount))
+    const uniqueBooksCount = bookSeries(bookList).length
+    if (booksCount === uniqueBooksCount) {
+        return BOOK_PRICE * booksCount * (1 - discount(uniqueBooksCount))
+    }
+    return BOOK_PRICE * uniqueBooksCount * (1 - discount(uniqueBooksCount)) +
+        (booksCount - uniqueBooksCount) * BOOK_PRICE
 }
