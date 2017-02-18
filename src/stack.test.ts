88d16aa04f3e6b116e47847df13268ed7ba4b4a7
Silviu Eigel <sheigel@sheigel.local>
2017-02-18 10:06:29 +0200

    cover first 4 cases

--- src/stack.test.ts
+++ src/stack.test.ts
@@ -1,9 +1,34 @@
 import createStack from './stack'
 
 describe('Stack', () => {
-    test('can create empty stack', () => {
-        var stack = createStack()
-
+    var stack;
+    beforeEach(()=>{
+        stack = createStack()
+    })
+    test('can create stack object', () => {
         expect(stack).toBeTruthy()
     })
+    test(`newly created stack should be empty`, ()=>{
+        expect(stack.isEmpty()).toBeTruthy()
+    })
+    test(`newly created stack should have size 0`, ()=>{
+        expect(stack.getSize()).toBe(0)
+    })
+    test(`after one push stack should not be empty`, ()=>{
+        stack.push('test value')
+
+        expect(stack.isEmpty()).toBeFalsy()
+    })
+    test(`after one push stack size should be 1`, ()=>{
+        stack.push('test value')
+
+        expect(stack.getSize()).toBe(1)
+    })
+    test(`after one push and one pop stack should be empty`, ()=>{
+        stack.push('test value')
+        stack.pop()
+
+        expect(stack.isEmpty()).toBeTruthy()
+    })
+
 })
--- src/stack.ts
+++ src/stack.ts
@@ -1,3 +1,19 @@
 export default function createStack() {
-    return {}
+    var isEmpty = true
+    return {
+        isEmpty(){
+            return isEmpty
+        },
+        getSize(){
+            if (isEmpty) {
+                return 0
+            }
+            else {
+                return 1
+            }
+        },
+        push(param){
+            isEmpty = false
+        }
+    }
 }
